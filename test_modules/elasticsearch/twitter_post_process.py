9fcac083486fb6102494bd496395a8c14da0d9a2
Lorenzo Corti <corti.lorenz@gmail.com>
2020-03-30 12:31:31 +0200

    Added querying by tweet creation date

--- test_modules/elasticsearch/twitter_post_process.py
+++ test_modules/elasticsearch/twitter_post_process.py
@@ -2,6 +2,10 @@ import yaml
 import logging
 import os
 import requests
+import json
+import dateutil
+import pytz
+from datetime import datetime
 
 
 class TwitterPostProcess:
@@ -12,6 +16,7 @@ class TwitterPostProcess:
         #self.LOGGER = self.__get_logger()
         self.HOST = self.CONFIG['twitter_es']['host']
         self.INDEX = self.CONFIG['twitter_es']['index']
+        self.DATE = self.CONFIG['twitter_es']['last_time']
 
     def __get_logger(self):
         # create logger
@@ -30,25 +35,96 @@ class TwitterPostProcess:
         logger.addHandler(fh)
         return logger
 
-    def query_es(self, query, num_results):
-        req = self.HOST + self.INDEX + '/' + query
-        if num_results > 0:
-            req = req + '&size=' + str(num_results)
-        r = requests.get(req)
+    def query_es(self, query):
+        url = self.HOST + self.INDEX + "/_search"
+        r = requests.post(url, json=query)
         return r.json()
 
     def format_results(self, res):
-        # Sistemare
-        print(res['hits']['hits'][0]['_source']['text'])
+        # == Sistemare ==
+        hits = res['hits']['hits']
+        for h in hits:
+            print(h['_source']['text'])
+            print('='*10)
+        return hits
+
+    def get_last_post_date(self, data):
+        l = len(data)
+        if l > 0:
+            # Save the last tweet timestamp to resume
+            day = data[-1]["_source"]["created_at"]
+            print("Saving last timestamp...")
+            print(day)
+            return day
+        else:
+            # If nothing is found save the current time
+            # Adding timezone to match Elasticsearch date format
+            day = datetime.now()
+            timezone = pytz.timezone("Europe/Rome")
+            day_tz = timezone.localize(day)
+            day_tz_str = day_tz.strftime("%a %b %d %H:%M:%S %z %Y")
+            print("Saving last timestamp...")
+            print(day_tz_str)
+            return day_tz_str
+
+    def scheduled_test(self, query_args, num_results):
+        query_body = {}
+        if num_results > 0:
+            query_body["size"] = num_results
+        query_body["query"] = query_args
+        query_body["sort"] = {"created_at": "asc"}
+
+        # while not stop:
+        twitter_json = self.query_es(query_body)
+
+        twitter_hits = self.format_results(twitter_json)
+
+        self.DATE = self.get_last_post_date(twitter_hits)
+        self.CONFIG['twitter_es']['last_time'] = self.DATE
+        with open('./configuration.yaml', 'w') as f:
+            yaml.dump(self.CONFIG, f)
+
+        # to-do scheduling
 
     # Può essere superfluo per ora dato che abbiamo solo roba sul Corona
+
     def topic_extraction(self):
         pass
 
 
 if __name__ == '__main__':
     twitter_post_process = TwitterPostProcess()
-    query = '_search?q=favorited:false'
-    num_results = 5
-    twitter_json = twitter_post_process.query_es(query, num_results)
-    results = twitter_post_process.format_results(twitter_json)
+
+    query_simple = {
+        "range": {
+            "created_at": {
+                "format": "EEE MMM dd HH:mm:ss Z yyyy",
+                "gte": "now-14d",
+                "lt": "now"
+            }
+        }
+    }
+
+    query_not_fave = {
+        "bool": {
+            "must": [{
+                "term": {
+                    "favorited": "false"
+                }
+            }, {
+                "range": {
+                    "created_at": {
+                        "format": "EEE MMM dd HH:mm:ss Z yyyy",
+                        "gte": "now-14d",
+                        "lt": "now"
+                    }
+                }
+            }
+            ]
+        }
+    }
+
+    num_results = 0  # if 0 -> no 'size' in the request, elasticsearch defaults to 10 results
+    twitter_post_process.scheduled_test(query_simple, num_results)
+    #twitter_json = twitter_post_process.query_es(query, num_results)
+    #results = twitter_post_process.format_results(twitter_json)
