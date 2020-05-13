b3ee40edd7a1ca4029d95afe301b3ee34950fd3a
Silvio <silviopavanetto@gmail.com>
2020-05-13 17:16:09 +0200

    started to prepare post process of news main flow

--- post_processing/news_post_process.py
+++ post_processing/news_post_process.py
@@ -8,6 +8,7 @@ import json
 import os
 from scraping.news_scraper import NewsScraper
 from pymongo import MongoClient
+import dateparser
 
 
 class NewsPostProcess:
@@ -33,17 +34,44 @@ class NewsPostProcess:
 	def named_entity_recognition(self):
 		pass
 
-	def news_similarity(self):
-		# this is a task that can be performed only 
-		# after all the previous tasks because we have 
-		# to work not only on the text but also on features like topic and n.e.r. 
+	def news_analysis(self): 
 		pass
 
 	def main(self):
 		# this is the main workflow: here the extraction and processing 
 		# phases are looped until no other news has to be analyzed
-		pass
+		i = 0
+		for lang in self.CONFIG['collections_lang']:
+			if lang == 'it': # Just for testing. Remove it then.
+				if lang != 'it':
+					collection = self.MONGO_CLIENT['news']['article_' + lang] 
+				else:
+					collection = self.MONGO_CLIENT['news']['article']
+			
+			#print(self.CONFIG['post_process']['last_processed_id_en'] is None)
+			if i == 0:
+				for doc in collection.find({"id":self.CONFIG['post_process']['last_processed_id']}):
+					discover_date = doc['discoverDate']
+					discover_date = dateparser.parse(discover_date)#.strftime('%d/%m/%y, %H:%M')
+					res = collection.update_one({'id':doc['id']}, {"$set": { "discoverDate": discover_date }})
+					print(res)
+					i += 1
+				# print(doc)
+				# print('='*75)
+				# i += 1
+				# if i == 10:
+				# 	break
+		
 
+	def __stop(self, p, collection):
+		is_old_post = collection.find_one({'id_post': p['id_post']})
+		if is_old_post is None and p['timestamp'] >= self.min_date_post:
+			return False
+		# if p['timestamp'] >= self.min_date_post:
+		#     return False
+		else:
+			return True
+	
 	def __get_logger(self):
 		# create logger
 		logger = logging.getLogger('NewsPostProcess')
