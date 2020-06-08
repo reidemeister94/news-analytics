c903cf51042e032115ec5445772d8a074ff463f3
Lorenzo Corti <corti.lorenz@gmail.com>
2020-06-08 17:34:41 +0200

    test2

--- news_post_process.py
+++ news_post_process.py
@@ -19,6 +19,7 @@ import yaml
 
 class NewsPostProcess:
     def __init__(self):
+        # comment
         with open("configuration/configuration.yaml") as f:
             self.CONFIG = yaml.load(f, Loader=yaml.FullLoader)
         self.LOGGER = self.__get_logger()
@@ -70,7 +71,7 @@ class NewsPostProcess:
         return doc
 
     def format_topic_list(self, topics):
-        return [(word,float(weight)) for word,weight in topics]
+        return [(word, float(weight)) for word, weight in topics]
 
     def topic_extraction(self, doc, update_model):
         parsed_text = self.nlp_utils.parse_text(doc["text"])
@@ -84,7 +85,10 @@ class NewsPostProcess:
         for el in self.lda_module.model[
             self.lda_module.dictionary.doc2bow(parsed_text)
         ]:
-            document_topic_info[str(el[0])] = [float(round(el[1], 2)), self.format_topic_list(topics[el[0]][1])]
+            document_topic_info[str(el[0])] = [
+                float(round(el[1], 2)),
+                self.format_topic_list(topics[el[0]][1]),
+            ]
         doc["topic_extraction"] = document_topic_info
 
         if update_model:
@@ -97,7 +101,9 @@ class NewsPostProcess:
             labels,
             items,
             most_common_items,
-        ) = self.named_entity_recognition.named_entity_recognition_process(doc["text"])
+        ) = self.named_entity_recognition.named_entity_recognition_process(
+            doc["text"]
+        )
         ner_data = [elem_pos_type, labels, items, most_common_items]
         doc["named_entity_recognition"] = ner_data
         return doc
@@ -129,22 +135,31 @@ class NewsPostProcess:
         self.news_analyzer = NewsAnalyzer(self.CONFIG)
         self.lda_module = LdaModule(lang=lang, trained=True)
         self.nlp_utils = NLPUtils(lang=lang)
-        self.named_entity_recognition = NamedEntityRecognition(self.nlp_utils.nlp)
+        self.named_entity_recognition = NamedEntityRecognition(
+            self.nlp_utils.nlp
+        )
 
     def main(self):
         # this is the main workflow: here the extraction and processing
         # phases are looped until no other news has to be analyzed
         for lang in self.CONFIG["collections_lang"]:
-            self.LOGGER.info("Initializing core modules and extract news from db...")
+            self.LOGGER.info(
+                "Initializing core modules and extract news from db..."
+            )
             self.init_core_modules(lang)
             collection, not_processed_docs = self.db_news_extraction(lang)
-            self.LOGGER.info("Core modules initialized and news from db extracted...")
+            self.LOGGER.info(
+                "Core modules initialized and news from db extracted..."
+            )
             # print(len(list(not_processed_docs)))
             i = 0
             for doc in not_processed_docs:
                 if i % 100 == 0:
                     pprint(doc)
-                if self.batch_size == self.CONFIG["topic_extraction"]["batch_size"]:
+                if (
+                    self.batch_size
+                    == self.CONFIG["topic_extraction"]["batch_size"]
+                ):
                     updated_doc = self.process_doc(doc, update_model=True)
                     self.batch_size = 0
                     self.batch_docs.clear()
@@ -176,7 +191,9 @@ class NewsPostProcess:
         fh = logging.FileHandler(log_path)
         fh.setLevel(logging.DEBUG)
         # create formatter
-        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        formatter = logging.Formatter(
+            "%(asctime)s - %(levelname)s - %(message)s"
+        )
         fh.setFormatter(formatter)
         logger.addHandler(fh)
         return logger
