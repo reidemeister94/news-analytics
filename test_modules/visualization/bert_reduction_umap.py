82ee8cd6ca191a44b36c9dca95fcf0a2fc2ced63
Lorenzo Corti <corti.lorenz@gmail.com>
2021-01-08 10:14:46 +0100

    UMAP for dimensionality reduction & fixes to PCA + t-SNE version

--- test_modules/visualization/bert_reduction_umap.py
+++ test_modules/visualization/bert_reduction_umap.py
@@ -1,13 +1,12 @@
 from pymongo import MongoClient
 from datetime import datetime
-from sklearn.decomposition import PCA
-from sklearn.manifold import TSNE
 from os import path
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle5 as pickle
 import yaml
+import umap
 
 
 class NewsPostProcess:
@@ -29,27 +28,14 @@ class NewsPostProcess:
         else:
             name_coll = "article"
         collection = self.MONGO_CLIENT["news"][name_coll]
-        not_processed_docs = collection.find(self.QUERY, no_cursor_timeout=True).limit(1000)
+        not_processed_docs = collection.find(self.QUERY, no_cursor_timeout=True)
         return collection, not_processed_docs
 
     def reduce_dim(self, docs, limit):
-        # n_components must be between 0 and min(n_samples, n_features)
-        file_name = "./gianni.pickle"
-        if path.exists(file_name):
-            with open(file_name, "rb") as f:
-                pca = pickle.load(f)
-        else:
-            pca = PCA(n_components=limit, random_state=7)
-        res_pca = pca.fit_transform(docs)
-        print("Shape after PCA: ", res_pca.shape)
-        with open(file_name, "wb") as f:
-            pickle.dump(pca, f)
-        tsne = TSNE(
-            n_components=2, perplexity=10, random_state=6, learning_rate=1000, n_iter=1500
-        )
-        res_tsne = tsne.fit_transform(res_pca)
-        print("Shape after t-SNE: ", res_tsne.shape)
-        return res_tsne
+        reducer = umap.UMAP()
+        res_umap = reducer.fit_transform(docs)
+        print("Shape after UMAP: ", res_umap.shape)
+        return res_umap
 
     def main(self):
         lang = "en"
