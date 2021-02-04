41e3505c13e703db6b0558bc8916cf5f1c63142d
Lorenzo Corti <corti.lorenz@gmail.com>
2021-02-04 12:09:33 +0100

    Updated fix_topic_modeling to support monthly topic models

--- fix_topic_modeling.py
+++ fix_topic_modeling.py
@@ -1,14 +1,12 @@
+from datetime import datetime
 from pymongo import MongoClient
-from pymongo.errors import CursorNotFound, ServerSelectionTimeoutError
 from core_modules.topic_extraction.nlp_utils import NLPUtils
 from core_modules.topic_extraction.lda_module import LdaModule
 import os
-import time
 import logging
 import yaml
 import sys
 import gc
-import pprint
 
 
 class FixTopicProcess:
@@ -22,31 +20,57 @@ class FixTopicProcess:
         self.MONGO_CLIENT = MongoClient(mongourl)
         self.lda_module = None
         self.nlp_utils = None
-        self.batch_size = 0
-        self.batch_docs = []
-        self.QUERY = {
-            "$or": [{"processedEncoding": False}, {"processedEncoding": {"$exists": False}}]
-        }
 
-    def db_news_extraction(self, lang):
+    def build_query(self, start, end):
+        q = {"discoverDate": {"$gte": start, "$lt": end}}
+        return q
+
+    def yield_rows(self, cursor, chunk_size):
+        """
+        Generator to yield chunks from cursor
+        :param cursor:
+        :param chunk_size:
+        :return:
+        """
+        chunk = []
+        for i, row in enumerate(cursor):
+            if i % chunk_size == 0 and i > 0:
+                yield chunk
+                del chunk[:]
+            chunk.append(row)
+        yield chunk
+
+    def update_dates(self, start, end):
+        start_month = start.month + 1
+        start_year = start.year
+        end_month = end.month + 1
+        end_year = end.year
+        if start_month == 13:
+            start_month = 1
+            start_year = start.year + 1
+        if end_month == 13:
+            end_month = 1
+            end_year = end.year + 1
+        new_start = datetime(start_year, start_month, 1, 0, 0)
+        new_end = datetime(end_year, end_month, 1, 0, 0)
+        return new_start, new_end
+
+    def db_news_extraction(self, lang, query, chunk_size, limit=0):
         if lang != "it":
             name_coll = "article_" + lang
         else:
             name_coll = "article"
         collection = self.MONGO_CLIENT["news"][name_coll]
-        not_processed_docs = collection.find(self.QUERY, no_cursor_timeout=True)
-        # .sort([("$natural", -1)])
+        not_processed_docs = collection.find(
+            query, no_cursor_timeout=True, batch_size=chunk_size
+        ).limit(limit)
         return collection, not_processed_docs
 
-    def process_doc(self, doc, current_lang, update_model=False):
-        doc["text"] = doc["text"][:10000]  # temp fix ^ 2
-
+    def process_doc(self, doc):
+        # doc["text"] = doc["text"][:10000]  # temp fix ^ 2
         # topic extraction phase
-        # start_time = time.time()
         try:
-            # print("topic extraction started")
-            doc = self.topic_extraction(doc, update_model)
-            # print("topic extraction completed")
+            doc = self.topic_extraction(doc)
         except Exception:
             exc_type, _, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
@@ -55,47 +79,37 @@ class FixTopicProcess:
             )
             return None, "error"
         gc.collect()
-        # print("--- %s seconds for topic extraction ---" % (time.time() - start_time))
 
         return doc, None
 
-    def topic_extraction(self, doc, update_model):
+    def topic_extraction(self, doc):
         parsed_text = self.nlp_utils.parse_text(doc)
-        self.batch_docs.append(parsed_text)
+
         try:
             topics = self.lda_module.model.show_topics(
                 formatted=False,
-                num_topics=self.CONFIG["topic_extraction"]["num_topics"],
-                num_words=self.CONFIG["topic_extraction"]["num_words"],
+                num_topics=self.CONFIG["topic_extraction"]["model_params"]["num_topics"],
+                num_words=self.CONFIG["topic_extraction"]["model_params"]["num_words"],
             )
         except Exception:
             exc_type, _, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            # print("{}, {}, {}, {}".format(doc["_id"], exc_type, fname, exc_tb.tb_lineno))
             self.LOGGER.error(
                 "{}, {}, {}, {}".format(doc["_id"], exc_type, fname, exc_tb.tb_lineno)
             )
             raise Exception("Error on lda module show topics")
-        document_topic_info = []  # old: {}
+
+        # Formatting
+        document_topic_info = []
         for el in self.lda_module.model[self.lda_module.dictionary.doc2bow(parsed_text)]:
             new_entry = {}
-            new_entry["topic_number"] = str(el[0])  # 0 -> topics number
+            new_entry["topic_number"] = str(el[0])
             new_entry["topic_prob"] = float(round(el[1], 2))
-            # list of topics -> current topic (el[0]) -> list of words (el[0][1])
             new_entry["topic_tokens"] = self.lda_module.format_topic_list(topics[el[0]][1])
             document_topic_info.append(new_entry)
-        # doc["test_topic_extraction"] = document_topic_info
+
         doc["topic_extraction"] = document_topic_info
         doc["parsed_text"] = " ".join(word for word in parsed_text)
-        if update_model:
-            self.lda_module.update_lda_model(self.batch_docs)
-            # pprint.pprint(
-            #     self.lda_module.model.show_topics(
-            #         formatted=False,
-            #         num_topics=self.CONFIG["topic_extraction"]["num_topics"],
-            #         num_words=self.CONFIG["topic_extraction"]["num_words"],
-            #     )
-            # )
         return doc
 
     def db_news_update(self, collection, doc, empty=False):
@@ -114,82 +128,67 @@ class FixTopicProcess:
             }
         collection.update_one(query, newvalues)
 
-    def init_core_modules(self, lang):
-        self.lda_module = None
-        self.nlp_utils = None
-        self.lda_module = LdaModule(lang=lang, trained=True)
-        self.nlp_utils = NLPUtils(lang=lang)
-
-        # print("== Initial topics ==")
-        # pprint.pprint(
-        #     self.lda_module.model.show_topics(
-        #         formatted=False,
-        #         num_topics=self.CONFIG["topic_extraction"]["num_topics"],
-        #         num_words=self.CONFIG["topic_extraction"]["num_words"],
-        #     )
-        # )
-
     def main(self):
-        # this is the main workflow: here the extraction and processing
-        # phases are looped until no other news has to be analyzed
         self.LOGGER.info("=" * 120)
         self.LOGGER.info("STARTED POST PROCESSING")
-        lang = "nl"
-        # for lang in self.CONFIG["collections_lang"]:
-        self.LOGGER.info("CURRENT COLLECTION: ARTICLE {}".format(lang.upper()))
-        self.LOGGER.info("Initializing core modules and extract news from db...")
-        self.init_core_modules(lang)
-        stop = False
-        while not stop:
-            collection, not_processed_docs = self.db_news_extraction(lang)
-            not_processed_docs_count = collection.count_documents(self.QUERY)
-            self.LOGGER.info("{} Articles to analyze...".format(not_processed_docs_count))
-            if not_processed_docs_count < 100:
-                stop = True
-                not_processed_docs.close()
-                self.LOGGER.info(
-                    "Less than 100 articles to analyze ({} to be precise), \
-                        proceeding to next collection...".format(
-                        not_processed_docs_count
-                    )
+
+        chunk_size = 5000
+
+        for lang in self.CONFIG["collections_lang"]:
+            self.LOGGER.info("CURRENT COLLECTION: ARTICLE {}".format(lang.upper()))
+            self.LOGGER.info("Initializing core modules and extract news from db...")
+
+            start_year = 2020
+            start_month = 1
+            end_year = 2020
+            end_month = 2
+            start = datetime(start_year, start_month, 1, 0, 0)
+            end = datetime(end_year, end_month, 1, 0, 0)
+
+            self.nlp_utils = None
+            self.nlp_utils = NLPUtils(lang=lang)
+
+            while end.year <= 2020:
+                query = self.build_query(start, end)
+                collection, not_processed_docs = self.db_news_extraction(
+                    lang, query, chunk_size, limit=100
                 )
-                break
-            self.LOGGER.info("Core modules initialized and news from db extracted...")
-            # print(len(list(not_processed_docs)))
-            i = 0
-            self.LOGGER.info("Starting processing docs from db...")
-            try:
-                for doc in not_processed_docs:
-                    # start_time = time.time()
-                    # print(i)
-                    if i % 100 == 0 and i > 0:
-                        print(i)
-                    if i % 1000 == 0 and i > 0:
-                        gc.collect()
-                    if i % 10000 == 0 and i > 0:
-                        self.LOGGER.info("10k Docs processed...")
-                    if len(doc["text"]) > 0 and not doc["text"].isspace():
-                        if self.batch_size == self.CONFIG["topic_extraction"]["batch_size"]:
-                            # print("== Updated topics ==")
-                            updated_doc, error = self.process_doc(doc, lang, update_model=True)
-                            self.batch_size = 0
-                            self.batch_docs.clear()
+
+                # Create empty than load
+                self.lda_module = None
+                self.lda_module = LdaModule()
+                self.lda_module.load_lda_model(lang, start)
+                # print("Topics for {}".format(start.strftime("%b_%Y")))
+                # t = self.lda_module.model.show_topics(
+                #     formatted=True,
+                #     num_topics=self.CONFIG["topic_extraction"]["model_params"]["num_topics"],
+                #     num_words=self.CONFIG["topic_extraction"]["model_params"]["num_words"],
+                # )
+                # print(t)
+                # print("=" * 20)
+
+                i = 0
+                self.LOGGER.info(
+                    "Starting processing docs from {}".format(start.strftime("%b_%Y"))
+                )
+                chunks = self.yield_rows(not_processed_docs, chunk_size)
+                for chunk in chunks:
+                    for doc in chunk:
+                        if i % 1000 == 0 and i > 0:
+                            print(i)
+                            gc.collect()
+                        if i % 10000 == 0 and i > 0:
+                            self.LOGGER.info("10k Docs processed...")
+                        if len(doc["text"]) > 0 and not doc["text"].isspace():
+                            updated_doc, error = self.process_doc(doc)
+
+                            if error is None:
+                                self.db_news_update(collection, updated_doc, empty=False)
                         else:
-                            updated_doc, error = self.process_doc(
-                                doc, lang, update_model=False
-                            )
-                        if error is None:
-                            self.batch_size += 1
-                            self.db_news_update(collection, updated_doc, empty=False)
-                            # print("DOC UPDATED TO DB!")
-                            i += 1
-                    # print("--- %s seconds ---" % (time.time() - start_time))
-                    else:
-                        self.db_news_update(collection, doc, empty=True)
-            except (CursorNotFound, ServerSelectionTimeoutError) as e:
-                print(type(e))
-                self.LOGGER.error("Lost cursor. Retry...")
-            not_processed_docs.close()
+                            self.db_news_update(collection, doc, empty=True)
+                        i = i + 1
+                start, end = self.update_dates(start, end)
+                not_processed_docs.close()
 
     def __get_logger(self):
         # create logger
