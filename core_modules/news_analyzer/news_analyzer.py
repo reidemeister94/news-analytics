b43bf1c7937c012bcc7be59296b981329882030e
Silvio <silvio.pavanetto@gmail.com>
2021-06-11 00:26:39 +0200

    fixes to news encoding with bert

--- core_modules/news_analyzer/news_analyzer.py
+++ core_modules/news_analyzer/news_analyzer.py
@@ -32,7 +32,7 @@ class NewsAnalyzer:
         self.BERT_MODEL = TFBertModel.from_pretrained("bert-base-multilingual-cased")
         self.LOGGER.info("=" * 120)
         self.LOGGER.info("Bert client ready")
-        self.MAX_LENGTH = 40
+        self.MAX_LENGTH = 64
 
     def text_rank(self, document):
         sentence_tokenizer = PunktSentenceTokenizer()
@@ -72,14 +72,22 @@ class NewsAnalyzer:
                         [
                             self.BERT_TOKENIZER.encode(
                                 text_rank[i][1],
+                                truncation=True,
                                 max_length=self.MAX_LENGTH,
-                                pad_to_max_length=True,
-                                padding_side="right",
+                                padding="max_length",
                             )
                         ]
                     )
-                    encoded = self.BERT_MODEL(encoded)[0]
+                    encoded = self.BERT_MODEL(encoded)
+                    # print("=" * 75)
+                    # print(encoded.keys())
+                    # print(encoded[0].shape)
+                    # print(encoded[1].shape)
+
+                    encoded = encoded[0]
                     encodings.append(tf.reduce_mean(encoded, axis=1))
+                    # print(tf.reduce_mean(encoded, axis=1).shape)
+                    # print("=" * 75)
                 res = np.average(encodings, axis=0)
                 final_res = []
                 for elem in res[0]:
@@ -116,6 +124,15 @@ class NewsAnalyzer:
 
 if __name__ == "__main__":
     news_analyzer = NewsAnalyzer()
-    doc = {"text": "the cat is on the table and the story ends here."}
-    enc = news_analyzer.encode_news(doc)
-    print(enc)
+    doc = {
+        "text": """
+È morta Camilla Canepa, la diciottenne di Sestri Levante ricoverata sabato al San Martino di Genova dopo una trombosi al seno cavernoso e operata per la rimozione del trombo e ridurre la pressione intracranica. Era stata vaccinata con il vaccino di AstraZeneca il 25 maggio nell'open day per gli over 18.
+
+"Purtroppo, poche ore fa, Sestri Levante è stata colpita da un lutto che mai avremmo voluto vivere - dice la sindaca di Sestri Valentina Ghio-. L'amministrazione comunale e tutta la città si stringono intorno alla famiglia della ragazza scomparsa oggi. In questo momento di dolore esprimo tutto il mio affetto e la mia vicinanza ai familiari di Camilla". E su Twitter Matteo Salvini : "Vaccini ai bimbi e ragazzi sconsigliati da Paesi europei, riviste scientifiche e medici. Stop, sulla salute dei nostri figli e nipoti non si scherza"
+
+La giovane era stata ricoverata al San Martino il 5 giugno per una trombosi dopo avere ricevuto il vaccino AstraZeneca dieci giorni prima e secondo quanto si ipotizza, forse avrebbe potuto essere operata già due giorni prima. È quanto vogliono verificare il pubblico ministero Stefano Puppo e il procuratore aggiunto Francesco Pinto che in un primo tempo hanno aperto un fascicolo senza ipotesi di reato sulla vicenda: il reato ipotizzabile sarebbe quello di lesioni colpose procedibile però solo a querela di parte. Dopo il decesso, però, il procuratore capo Francesco Cozzi annuncia l'indagine per omicidio colposo, al momento contro ignoti. In ogni caso, i magistrati hanno chiesto alla direzione sanitaria tutta la documentazione relativa all’iter vaccinale, ma anche a quanto successo dal primo accesso al pronto soccorso, passando per le dimissioni, e il secondo ricovero."""
+    }
+    res, triples_extraction_container = news_analyzer.encode_news(doc)
+    # print(res)
+    # print("&" * 75)
+    # print(triples_extraction_container)
